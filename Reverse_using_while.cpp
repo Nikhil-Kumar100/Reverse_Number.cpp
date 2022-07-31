c251e094c7ca7328302a86d3ba07404b28d6734e
Nikhil Kumar <109630112+Nikhil-Kumar100@users.noreply.github.com>
2022-07-31 22:39:18 +0530

    Reverse_using_while.cpp
    Reverse number using while loop

---
+++ Reverse_using_while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter Number :-  ";
+    cin>>num;
+    while(num>=0){
+        cout<<num<<endl;
+        num--;
+    }
+    // for(int i=num;i>=1;--i){
+    //     cout<<i<<endl;
+    // }
+    return 0;
+}
